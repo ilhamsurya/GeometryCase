97932106a5e914760e8e6ec49d4177d7aab26cc4
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-20 16:07:25 +0700

    displaying the result in main menu

---
+++ src/main/java/com/geometry/Main.java
@@ -0,0 +1,12 @@
+package com.geometry;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String arg[]){
+        System.out.println("test");
+        Point point = new Point(3,2);
+        Point pointb = new Point(-2, -5);
+        Line line = new Line(point, pointb);
+        line.calculateLength();
+    }
+}
