497d92ac49ee7b211552e7be03a3804208c704b9
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-20 16:04:12 +0700

    adding test case calculating line length

--- src/test/java/com/geometry/GeometryTest.java
+++ src/test/java/com/geometry/GeometryTest.java
@@ -11,7 +11,13 @@ public class GeometryTest {
         assertEquals(5,point.y);
     }
 
-
+    @Test
+    public void calculateLength(){
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(5, 0);
+        Line line = new Line(point1, point2);
+        assertEquals(5, line.calculateLength());
+    }
 
 }
 
